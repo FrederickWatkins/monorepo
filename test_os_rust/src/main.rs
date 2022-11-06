e88a7647535b33ad88b1f40e0d5e00fd2907f1fc
Frederick Watkins <freddiewatkins6@gmail.com>
2022-11-06 01:16:24 +0000

    Fixed rust os tests

--- test_os_rust/src/main.rs
+++ test_os_rust/src/main.rs
@@ -1,20 +1,28 @@
 #![no_std]
 #![no_main]
 #![feature(custom_test_frameworks)]
+#![test_runner(qemu_test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buffer;
+
+#[cfg(not(test))]
+use core::panic::PanicInfo;
 
 // The entry point for the OS
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     #[cfg(test)]
     test_main();
-    loop{}
+
+    println!("Yabba dabba doo!");
+    panic!("Test panic");
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
+    
     println!("{}", info);
     loop {}
 }
